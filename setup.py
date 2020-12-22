1be395cdeba7f618d350ae6897a8ad1df6233f3b
Kris <31852063+krisfris@users.noreply.github.com>
2020-12-22 13:44:33 +0200

    Add __init__.py to satisfy setuptools' find_packages

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='withtray',
-    version='0.1.0',
+    version='0.1.1',
     description='Run any blocking command with systray icon',
     long_description=long_description,
     long_description_content_type='text/markdown',
